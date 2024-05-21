e77d5215a3102b34146f46277513234204b01853
rabbit-313 <itoru03@gmail.com>
2024-05-21 16:17:08 +0900

    add: ensamble

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
-MESSAGE="stacking"
+MESSAGE="Ensamble of 3 models: XGBoost, LightGBM, CatBoost."
 COMPETITION="FloodPrediction"
 kaggle competitions submit -c playground-series-s4e5 -f "${COMPETITION}/outputs/submission_stacking.csv" -m "${MESSAGE}"
