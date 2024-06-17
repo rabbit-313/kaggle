7454e5f97976abd3858435740899246125c3c22e
rabbit-313 <itoru03@gmail.com>
2024-06-17 17:30:07 +0900

    add: AcademicSuccess

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
-MESSAGE="Ensamble of 3 models: XGBoost, LightGBM, CatBoost."
-COMPETITION="FloodPrediction"
-kaggle competitions submit -c playground-series-s4e5 -f "${COMPETITION}/outputs/submission_stacking.csv" -m "${MESSAGE}"
+MESSAGE="lightgbm + optuna"
+COMPETITION="AcademicSuccess"
+kaggle competitions submit -c playground-series-s4e6 -f "${COMPETITION}/outputs/submission.csv" -m "${MESSAGE}"
