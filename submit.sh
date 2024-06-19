3a5cc8ed4f77775c352e733abc5e0a66d240d0b2
rabbit-313 <itoru03@gmail.com>
2024-06-19 15:21:54 +0900

    add: some

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
-MESSAGE="lightgbm + optuna"
+MESSAGE="add feature tsne"
 COMPETITION="AcademicSuccess"
-kaggle competitions submit -c playground-series-s4e6 -f "${COMPETITION}/outputs/submission.csv" -m "${MESSAGE}"
+kaggle competitions submit -c playground-series-s4e6 -f "${COMPETITION}/outputs/submission_add_feature_tsne.csv" -m "${MESSAGE}"
